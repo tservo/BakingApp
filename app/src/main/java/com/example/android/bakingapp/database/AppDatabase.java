d1c5df99d7553f3cf416fe81484d253746d6a41e
Ryan Bacon <tservo@tservo.org>
2018-08-19 14:00:44 -0700

    App database is abstract!

--- app/src/main/java/com/example/android/bakingapp/database/AppDatabase.java
+++ app/src/main/java/com/example/android/bakingapp/database/AppDatabase.java
@@ -1,26 +1,6 @@
 package com.example.android.bakingapp.database;
 
-import android.arch.persistence.db.SupportSQLiteOpenHelper;
-import android.arch.persistence.room.DatabaseConfiguration;
-import android.arch.persistence.room.InvalidationTracker;
 import android.arch.persistence.room.RoomDatabase;
-import android.support.annotation.NonNull;
 
-public class AppDatabase extends RoomDatabase {
-    @NonNull
-    @Override
-    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
-        return null;
-    }
-
-    @NonNull
-    @Override
-    protected InvalidationTracker createInvalidationTracker() {
-        return null;
-    }
-
-    @Override
-    public void clearAllTables() {
-
-    }
+public abstract class AppDatabase extends RoomDatabase {
 }
